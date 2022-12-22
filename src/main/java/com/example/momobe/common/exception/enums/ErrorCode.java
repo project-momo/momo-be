4bb823699c42cf89febab6629a03aa3eb72e47cb
yangddoddi <pud1251@gmail.com>
2022-12-22 13:23:17 +0900

    update : 에러코드 추가

--- src/main/java/com/example/momobe/common/exception/enums/ErrorCode.java
+++ src/main/java/com/example/momobe/common/exception/enums/ErrorCode.java
@@ -3,10 +3,13 @@ package com.example.momobe.common.exception.enums;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Getter
 public enum ErrorCode {
+    REQUEST_CONFLICT(CONFLICT, "해당 요청을 수행할 수 없습니다, 관리자에게 문의하세요."),
+
     INVALID_TOKEN(UNAUTHORIZED, "invalid token exception"),
     SIGNATURE_EXCEPTION(UNAUTHORIZED, "signature key is different"),
     EXPIRED_EXCEPTION(UNAUTHORIZED, "expired token"),
