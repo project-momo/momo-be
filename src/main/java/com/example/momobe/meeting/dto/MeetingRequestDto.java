1fa14978298aed52386e218357b8bed64cf2bdb4
kkg5 <kkg05102@gmail.com>
2022-12-22 20:57:55 +0900

    feat : MeetingRequestDto > Validation 추가

--- src/main/java/com/example/momobe/meeting/dto/MeetingRequestDto.java
+++ src/main/java/com/example/momobe/meeting/dto/MeetingRequestDto.java
@@ -20,15 +20,19 @@ import static lombok.AccessLevel.PRIVATE;
 @AllArgsConstructor(access = PRIVATE)
 public class MeetingRequestDto {
     @Positive
+    @NotNull
     private Long categoryId;
     @NotBlank
     private String title;
     @NotBlank
     private String content;
     @Size(min = 1)
+    @NotNull
     private Set<Long> tagIds;
-    @Size(min = 1)
+    @Size(min = 1, max = 3)
+    @NotNull
     private List<LocationDto> locations;
+    @NotNull
     private PriceDto priceInfo;
     private String notice;
 
@@ -40,6 +44,7 @@ public class MeetingRequestDto {
         private String address1;
         private String address2;
         @Size(min = 1)
+        @NotNull
         private List<LocalDateTime> dateTimes;
     }
 
