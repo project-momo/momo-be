baeab8cf091e9ce6c95a94c276a0eaf8418fdf3f
yangddoddi <pud1251@gmail.com>
2022-12-22 21:32:47 +0900

    refactor : RandomPasswordGenerator 추상화로 계층 분리

---
+++ src/main/java/com/example/momobe/common/domain/BaseTime.java
@@ -0,0 +1,30 @@
+package com.example.momobe.common.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTime {
+    @CreatedDate
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(updatable = false,name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "last_modified_at")
+    private LocalDateTime lastModifiedAt;
+}
---
+++ src/main/java/com/example/momobe/common/domain/RedisStore.java
@@ -0,0 +1,10 @@
+package com.example.momobe.common.domain;
+
+import java.util.Optional;
+
+public interface RedisStore <T> {
+    void saveData(String key, T value, Long expire);
+    Optional<T> getData(String key, Class<T> type);
+    void deleteData(String key);
+    Optional<T> getDataAndDelete(String key, Class<T> type);
+}
