20c8bb344aedfbf71dffe707e776c0922291198a
yangddoddi <pud1251@gmail.com>
2022-12-23 10:43:04 +0900

    feat : 토큰 재발행 컨트롤러 로직 구현

--- src/main/java/com/example/momobe/user/ui/TokenController.java
+++ src/main/java/com/example/momobe/user/ui/TokenController.java
@@ -2,6 +2,8 @@ package com.example.momobe.user.ui;
 
 import com.example.momobe.security.enums.SecurityConstants;
 import com.example.momobe.user.application.LogoutService;
+import com.example.momobe.user.application.ReissueTokenService;
+import com.example.momobe.user.dto.JwtTokenDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +15,17 @@ import static com.example.momobe.security.enums.SecurityConstants.*;
 @RequiredArgsConstructor
 public class TokenController {
     private final LogoutService logoutService;
+    private final ReissueTokenService reissueTokenService;
 
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteToken(@RequestHeader(REFRESH_HEADER) String refreshToken) {
         logoutService.logout(refreshToken);
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public JwtTokenDto reissueToken(@RequestHeader(REFRESH_HEADER) String refreshToken) {
+        return reissueTokenService.reIssueToken(refreshToken);
     }
 }
