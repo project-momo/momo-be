b17f671882aa8a507978d254f0a16334c0e3434e
yangddoddi <pud1251@gmail.com>
2022-12-23 10:48:16 +0900

    update : 생성자 체이닝하고 userState를 기본으로 받도록 변경

--- src/main/java/com/example/momobe/user/domain/User.java
+++ src/main/java/com/example/momobe/user/domain/User.java
@@ -53,6 +53,16 @@ public class User extends BaseTime {
     @JoinColumn(name = "avatar_id")
     private Avatar avatar;
 
+    public User(String email, String nickname, String password, Avatar avatar) {
+        this.role = new Role(List.of(ROLE_USER));
+        this.email = new Email(email);
+        this.nickname = new Nickname(nickname);
+        this.password = new Password(password);
+        this.point = new Point(0L);
+        this.avatar = avatar;
+        this.userState = new UserState(UserStateType.ACTIVE, LocalDateTime.now());
+    }
+
     public User(String email, String nickname, String password, Avatar avatar,UserState userState) {
         this.role = new Role(List.of(ROLE_USER));
         this.userState = userState;
@@ -63,13 +73,8 @@ public class User extends BaseTime {
         this.avatar = avatar;
     }
 
-
     //private 으로 막아야 되는데 그럼 도메인 서비스에서 못쓰자너..
     public void setUserState(UserStateType userState, LocalDateTime dateTime) {
         this.userState = new UserState(userState,dateTime);
     }
-
-
-
-
 }
