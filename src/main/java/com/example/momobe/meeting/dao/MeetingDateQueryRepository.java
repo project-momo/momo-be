13f807d7cc0ef53d31154a3e017892753b591045
yangddoddi <pud1251@gmail.com>
2022-12-30 17:37:27 +0900

    feat : 모임 날짜 조회용 레포지토리 추가 (미완성)

---
+++ src/main/java/com/example/momobe/meeting/dao/MeetingDateQueryRepository.java
@@ -0,0 +1,33 @@
+package com.example.momobe.meeting.dao;
+
+import com.example.momobe.meeting.domain.DateTime;
+//import com.example.momobe.meeting.domain.QDateTime;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+
+import static com.example.momobe.meeting.domain.QDateTime.*;
+
+@Repository
+@RequiredArgsConstructor
+public class MeetingDateQueryRepository {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public List<DateTime> findMeetingDateBy(Long meetingId,
+                                            LocalDate localDate,
+                                            LocalTime startTime,
+                                            LocalTime endTime) {
+        return jpaQueryFactory.select(dateTime1)
+                .from(dateTime1)
+                .where(dateTime1.meeting.id.eq(meetingId)
+                        .and(dateTime1.dateTime.eq(LocalDateTime.of(localDate, startTime))
+                                .and(dateTime1.dateTime.goe(LocalDateTime.of(localDate, endTime)))))
+                .fetch();
+    }
+}
