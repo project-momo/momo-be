7381aa831ed0c16642f94b27780200f7a80afed0
kkg5 <kkg05102@gmail.com>
2022-12-30 19:02:41 +0900

    feat : enum > description 추가

--- src/main/java/com/example/momobe/meeting/domain/enums/Tag.java
+++ src/main/java/com/example/momobe/meeting/domain/enums/Tag.java
@@ -1,5 +1,17 @@
 package com.example.momobe.meeting.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Tag {
-    LIFESTYLE, MEDIA, EDU, SOCIAL, DESIGN, DEVELOP, FINANCE, AI
+    MENTORING("멘토링"),
+    ONLINE("온라인"),
+    OFFLINE("오프라인"),
+    STUDY("스터디"),
+    MEETING("모임"),
+    FIVE("5인 이상");
+
+    private final String description;
 }
