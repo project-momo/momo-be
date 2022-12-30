9944a0d5322db4cf137bf5d3deb4d79f67ea51a6
kkg5 <kkg05102@gmail.com>
2022-12-30 19:06:45 +0900

    feat : 모임 목록 조회 Dto 수정, 목 데이터 추가

--- src/main/java/com/example/momobe/meeting/dao/MeetingQueryRepository.java
+++ src/main/java/com/example/momobe/meeting/dao/MeetingQueryRepository.java
@@ -3,6 +3,7 @@ package com.example.momobe.meeting.dao;
 import com.example.momobe.meeting.domain.enums.Category;
 import com.example.momobe.meeting.dto.MeetingResponseDto;
 import com.example.momobe.meeting.dto.QMeetingResponseDto;
+import com.querydsl.core.group.Group;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -14,12 +15,18 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.example.momobe.address.domain.QAddress.address;
+import static com.example.momobe.meeting.domain.QDateTime.dateTime1;
 import static com.example.momobe.meeting.domain.QMeeting.meeting;
+import static com.example.momobe.reservation.domain.QReservation.reservation;
 import static com.example.momobe.user.domain.QAvatar.avatar;
 import static com.example.momobe.user.domain.QUser.user;
+import static com.querydsl.core.group.GroupBy.groupBy;
 
 @Repository
 @RequiredArgsConstructor
@@ -42,10 +49,32 @@ public class MeetingQueryRepository {
                         meeting.address.addressInfo,
                         meeting.meetingState,
                         meeting.dateTimeInfo.datePolicy,
-                        meeting.price,
-                        meeting.notice))
+                        meeting.dateTimeInfo.startDate,
+                        meeting.dateTimeInfo.endDate,
+                        meeting.dateTimeInfo.startTime,
+                        meeting.dateTimeInfo.endTime,
+                        meeting.dateTimeInfo.maxTime,
+                        meeting.price))
                 .where(containsKeyword(keyword), eqCategory(category))
                 .fetch();
+
+//        List<Long> meetingIds = dtos.stream()
+//                .map(MeetingResponseDto::getMeetingId).collect(Collectors.toList());
+//
+//        Map<Long, Group> groupMap = queryFactory
+//                .from(meeting)
+//                .where(meeting.id.in(meetingIds))
+//                .innerJoin(reservation).on(reservation.meetingId.in(meetingIds))
+//                .innerJoin(user).on(reservation.reservedUser.userId.eq(user.id))
+//                .innerJoin(user.avatar, avatar)
+//                .innerJoin(address).on(address.id.in(meeting.address.addressIds))
+//                .innerJoin(meeting.dateTimeInfo.dateTimes, dateTime1)
+//                .transform(groupBy(meeting.id).as(address.gu, address.si, dateTime1.dateTime));
+
+        dtos.forEach(dto -> dto.init(
+                // 목데이터
+                List.of("서울시 강남구", "서울시 강북구"),
+                List.of(1, 3, 7), List.of(LocalDate.now(), LocalDate.now().plusDays(1))));
 
         JPAQuery<Long> countQuery = queryFactory
                 .select(meeting.count())
