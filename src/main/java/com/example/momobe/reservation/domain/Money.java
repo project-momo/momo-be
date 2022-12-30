7279d5f6ddfca502ccca86688cf6ce378e1d96df
yangddoddi <pud1251@gmail.com>
2022-12-30 17:27:21 +0900

    update : get 메서드 추가

--- src/main/java/com/example/momobe/reservation/domain/Money.java
+++ src/main/java/com/example/momobe/reservation/domain/Money.java
@@ -4,13 +4,20 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
 import javax.persistence.Embeddable;
 
 import static lombok.AccessLevel.*;
 
 @Embeddable
 @AllArgsConstructor
+@Access(AccessType.FIELD)
 @NoArgsConstructor(access = PROTECTED)
 public class Money {
     private Long won;
+
+    public Long getWon() {
+        return this.won;
+    };
 }
