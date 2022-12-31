0bff48ffbfe1eb606049541f52367a4a2ac759c7
kkg5 <kkg05102@gmail.com>
2022-12-31 17:42:32 +0900

    test : 참여 모임 목록 조회 테스트 작성 + API 문서 갱신

--- src/test/java/com/example/momobe/meeting/ui/MeetingMyPageQueryControllerTest.java
+++ src/test/java/com/example/momobe/meeting/ui/MeetingMyPageQueryControllerTest.java
@@ -4,9 +4,9 @@ import com.example.momobe.common.config.SecurityTestConfig;
 import com.example.momobe.common.resolver.JwtArgumentResolver;
 import com.example.momobe.meeting.dao.MeetingHostQueryRepository;
 import com.example.momobe.meeting.dao.MeetingParticipantQueryRepository;
-import com.example.momobe.meeting.domain.enums.Category;
 import com.example.momobe.meeting.domain.enums.DatePolicy;
 import com.example.momobe.meeting.dto.MeetingHostResponseDto;
+import com.example.momobe.meeting.dto.MeetingParticipantResponseDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -40,6 +40,7 @@ import static org.springframework.restdocs.payload.JsonFieldType.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MeetingMyPageQueryController.class)
@@ -124,14 +125,78 @@ public class MeetingMyPageQueryControllerTest {
                                 fieldWithPath("content[].applications.confirmed[].imageUrl").type(STRING).description("지원자 이미지"),
                                 fieldWithPath("content[].applications.confirmed[].reservationState").type(STRING).description("예약 상태"),
                                 fieldWithPath("content[].applications.confirmed[].email").type(STRING).description("지원자 이메일"),
+                                fieldWithPath("content[].applications.confirmed[].message").type(STRING).description("전달 사항"),
                                 fieldWithPath("content[].applications.confirmed[].dateTimeInfo").type(OBJECT).description("날짜 정보"),
                                 fieldWithPath("content[].applications.confirmed[].dateTimeInfo.date").type(STRING).description("예약 날짜"),
                                 fieldWithPath("content[].applications.confirmed[].dateTimeInfo.time").type(STRING).description("예약 시간"),
-                                fieldWithPath("content[].applications.confirmed[].message").type(STRING).description("전달 사항"),
 
                                 FWP_PAGE_INFO, FWP_PAGE, FWP_SIZE, FWP_TOTAL_ELEMENTS, FWP_TOTAL_PAGES
                         )
+                ));
+    }
 
+    @Test
+    public void meetingParticipantQuery() throws Exception {
+        // given
+        MeetingParticipantResponseDto.ApplicationDto applicationDto =
+                new MeetingParticipantResponseDto.ApplicationDto(
+                        ID2, NICKNAME1, REMOTE_PATH, PAYMENT_SUCCESS,
+                        EMAIL1, "잘 부탁드려요~",
+                        new MeetingParticipantResponseDto.DateTimeInfo(
+                                LocalDate.now(), START_TIME, END_TIME));
+        MeetingParticipantResponseDto meetingParticipantResponseDto = new MeetingParticipantResponseDto(
+                ID1, SOCIAL, ID1, NICKNAME, REMOTE_PATH, TITLE1, CONTENT1, ADDRESS1, OPEN,
+                DatePolicy.FREE, START_DATE, END_DATE, START_TIME, END_TIME, 3, 1000L, applicationDto
+        );
+        meetingParticipantResponseDto.init(
+                List.of("서울시 강남구", "서울시 강북구"),
+                List.of(1, 3, 7), List.of(LocalDate.now(), LocalDate.now().plusDays(1)));
+        PageRequest pageRequest = PageRequest.of(PAGE - 1, SIZE);
+
+        given(meetingParticipantQueryRepository.findAll(any(), any(Pageable.class)))
+                .willReturn(new PageImpl<>(List.of(meetingParticipantResponseDto), pageRequest, 1L));
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                get("/mypage/meetings/participants")
+                        .header(JWT_HEADER, BEARER_ACCESS_TOKEN)
+                        .param("page", String.valueOf(PAGE))
+                        .param("size", String.valueOf(SIZE))
+        );
+
+        // then
+        actions.andExpect(status().isOk())
+                .andDo(document("mypage/meetings/participants",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        REQUEST_HEADER_JWT,
+                        requestParameters(
+                                PWN_PAGE, PWN_SIZE
+                        ),
+                        responseFields(
+                                FWP_CONTENT, FWP_CONTENT_MEETING_ID, FWP_CONTENT_CATEGORY,
+                                FWP_CONTENT_HOST, FWP_CONTENT_HOST_USER_ID, FWP_CONTENT_HOST_NICKNAME, FWP_CONTENT_HOST_IMAGE_URL,
+                                FWP_CONTENT_TITLE, FWP_CONTENT_CONTENT,
+                                FWP_CONTENT_ADDRESS, FWP_CONTENT_ADDRESS_ADDRESSES, FWP_CONTENT_ADDRESS_ADDRESS_INFO,
+                                FWP_CONTENT_MEETING_STATE, FWP_CONTENT_IS_OPEN,
+                                FWP_CONTENT_DATE_TIME, FWP_CONTENT_DATE_TIME_DATE_POLICY, FWP_CONTENT_DATE_TIME_START_DATE,
+                                FWP_CONTENT_DATE_TIME_END_DATE, FWP_CONTENT_DATE_TIME_START_TIME, FWP_CONTENT_DATE_TIME_END_TIME,
+                                FWP_CONTENT_DATE_TIME_MAX_TIME, FWP_CONTENT_DATE_TIME_DAY_WEEKS, FWP_CONTENT_DATE_TIME_DATES,
+                                FWP_CONTENT_PRICE,
+
+                                fieldWithPath("content[].application").type(OBJECT).description("지원 정보"),
+                                fieldWithPath("content[].application.userId").type(NUMBER).description("지원자 식별자"),
+                                fieldWithPath("content[].application.nickname").type(STRING).description("지원자 닉네임"),
+                                fieldWithPath("content[].application.imageUrl").type(STRING).description("지원자 이미지"),
+                                fieldWithPath("content[].application.reservationState").type(STRING).description("예약 상태"),
+                                fieldWithPath("content[].application.email").type(STRING).description("이메일"),
+                                fieldWithPath("content[].application.message").type(STRING).description("전달 사항"),
+                                fieldWithPath("content[].application.dateTimeInfo").type(OBJECT).description("날짜 정보"),
+                                fieldWithPath("content[].application.dateTimeInfo.date").type(STRING).description("예약 날짜"),
+                                fieldWithPath("content[].application.dateTimeInfo.time").type(STRING).description("예약 시간"),
+
+                                FWP_PAGE_INFO, FWP_PAGE, FWP_SIZE, FWP_TOTAL_ELEMENTS, FWP_TOTAL_PAGES
+                        )
                 ));
     }
 
