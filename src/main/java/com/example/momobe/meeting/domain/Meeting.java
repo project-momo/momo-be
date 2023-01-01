16dae53d130cb6b44d762e2e68f7af524ecfe06d
yangddoddi <pud1251@gmail.com>
2023-01-01 18:23:57 +0900

    update : 금액 정합성 계산용 메서드 추가

--- src/main/java/com/example/momobe/meeting/domain/Meeting.java
+++ src/main/java/com/example/momobe/meeting/domain/Meeting.java
@@ -12,6 +12,7 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Objects;
 
 import static javax.persistence.EnumType.STRING;
 import static javax.persistence.GenerationType.IDENTITY;
@@ -89,5 +90,16 @@ public class Meeting extends BaseTime {
                                              LocalTime startTime,
                                              LocalTime endTime) {
         return dateTimeInfo.match(date, startTime, endTime);
+    }
+
+    public Boolean matchPrice(Long price,
+                              LocalTime startTime,
+                              LocalTime endTime) {
+        if (this.dateTimeInfo.hasFreePolish()) {
+            int period = endTime.getHour() - startTime.getHour();
+            return Objects.equals(period * this.price, price);
+        }
+
+        return Objects.equals(this.price, price);
     }
 }
