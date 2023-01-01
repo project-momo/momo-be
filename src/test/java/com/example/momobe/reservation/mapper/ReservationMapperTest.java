66c3f044a08338ac3513a8fa9a46512a61706834
yangddoddi <pud1251@gmail.com>
2023-01-01 18:32:51 +0900

    test : reservation mapper test 추가

--- src/test/java/com/example/momobe/reservation/mapper/ReservationMapperTest.java
+++ src/test/java/com/example/momobe/reservation/mapper/ReservationMapperTest.java
@@ -5,8 +5,10 @@ import com.example.momobe.common.resolver.UserInfo;
 import com.example.momobe.meeting.domain.DateTime;
 import com.example.momobe.meeting.domain.DateTimeInfo;
 import com.example.momobe.meeting.domain.Meeting;
+import com.example.momobe.reservation.domain.Money;
 import com.example.momobe.reservation.domain.Reservation;
 import com.example.momobe.reservation.dto.in.RequestReservationDto;
+import com.example.momobe.reservation.dto.out.ReservationPaymentDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,7 +31,7 @@ class ReservationMapperTest {
     }
 
     @Test
-    void of() {
+    void of1() {
         //given
         Meeting meeting = Meeting.builder()
                 .hostId(ID1)
@@ -64,5 +66,38 @@ class ReservationMapperTest {
         assertThat(result.getReservationDate().getEndTime()).isEqualTo(dto.getDateInfo().getEndTime());
         assertThat(result.getReservationMemo().getContent()).isEqualTo(dto.getReservationMemo());
         assertThat(result.getReservedUser().getUserId()).isEqualTo(user.getId());
+    }
+
+    @Test
+    void of2() {
+        //given
+        Meeting meeting = Meeting.builder()
+                .title(CONTENT1)
+                .dateTimeInfo(DateTimeInfo.builder()
+                        .dateTimes(List.of(new DateTime(LocalDateTime.now())))
+                        .build())
+                .build();
+
+        Reservation reservation = Reservation.builder()
+                .amount(new Money(10000L))
+                .id(ID1)
+                .build();
+
+        UserInfo user = UserInfo.builder()
+                .id(ID1)
+                .nickname(NICKNAME1)
+                .email(EMAIL1)
+                .build();
+
+        //when
+        ReservationPaymentDto result = reservationMapper.of(user, reservation, meeting);
+
+        //then
+        assertThat(result.getAmount()).isEqualTo(reservation.getAmount().getWon());
+        assertThat(result.getCustomerEmail()).isEqualTo(user.getEmail());
+        assertThat(result.getCustomerName()).isEqualTo(user.getNickname());
+        assertThat(result.getUserId()).isEqualTo(user.getId());
+        assertThat(result.getReservationID()).isEqualTo(reservation.getId());
+        assertThat(result.getOrderName()).isEqualTo(meeting.getTitle());
     }
 }
