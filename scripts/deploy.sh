8a7bf6b1e0611917fbd32879625a7d3a85253208
ilyadelavie <102658715+gangdodan@users.noreply.github.com>
2023-01-03 20:58:15 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ sudo docker rmi momoadmin/momo:1.0
 sudo docker pull momoadmin/momo:1.0
 
 # 도커 run
-docker run -d -p 8080:8080 --env-file ./sample.env -v /home/ec2-user:/config --name momo momoadmin/momo:1.0
+docker run -d -p 8080:8080 -v /home/ec2-user:/config --name momo momoadmin/momo:1.0
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않음
 docker rmi -f $(docker images -f "dangling=true" -q) || true
