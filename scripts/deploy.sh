71ee2d028a9557474fdbbb05b390653bc9241473
ilyadelavie <102658715+gangdodan@users.noreply.github.com>
2023-01-04 00:13:42 +0900

    chore : 이미지,컨테이너 이름 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
 # 가동중인 도커 중단 및 삭제
-sudo docker ps -a -q --filter "name=momo" | grep -q . && docker stop momo && docker rm momo | true
+sudo docker ps -a -q --filter "name=momoproject" | grep -q . && docker stop momoproject && docker rm momoproject | true
 
 # 기존 이미지 삭제
-sudo docker rmi momoadmin/momo:1.0
+sudo docker rmi dodannnn/momoproject:1.0
 
 # 도커허브 이미지 pull
-sudo docker pull momoadmin/momo:1.0
+sudo docker pull dodannnn/momoproject:1.0
 
 # 도커 run
-docker run -d -p 8080:8080 -v /home/ec2-user:/config --name momo momoadmin/momo:1.0 --env-file ./sample.env dodannnn/momoproject:1.0
+docker run -d -p 8080:8080 -v /home/ec2-user:/config --name momoproject dodannnn/momoproject:1.0 --env-file ./sample.env dodannnn/momoproject:1.0
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않음
 docker rmi -f $(docker images -f "dangling=true" -q) || true
