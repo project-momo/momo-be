92b72171806b8a31ddcd681e8d4d66d4d5978e04
yangddoddi <pud1251@gmail.com>
2023-01-04 21:00:49 +0900

    feat : 예약 가능일 조회 컨트롤러 계층 구현

---
+++ src/main/java/com/example/momobe/meeting/ui/MeetingDatesQueryController.java
@@ -0,0 +1,41 @@
+package com.example.momobe.meeting.ui;
+
+import com.example.momobe.common.exception.CanNotConvertException;
+import com.example.momobe.common.exception.enums.ErrorCode;
+import com.example.momobe.meeting.dao.MeetingDatesQueryMapper;
+import com.example.momobe.meeting.dto.ResponseMeetingDatesDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
+import static com.example.momobe.common.exception.enums.ErrorCode.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/meetings")
+public class MeetingDatesQueryController {
+    private final MeetingDatesQueryMapper meetingDatesQueryMapper;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{meetingId}/reservations/dates/{date}")
+    public List<ResponseMeetingDatesDto> getDates(@PathVariable(name = "meetingId") Long meetingId,
+                                                  @PathVariable(name = "date") String date) {
+        LocalDate localDate = convertLocalDate(date);
+        return meetingDatesQueryMapper.getTimes(meetingId, localDate.getMonthValue());
+    }
+
+    private LocalDate convertLocalDate(String date) {
+        try {
+            return Date.valueOf(date).toLocalDate();
+        } catch (IllegalArgumentException e) {
+            log.error("date format ex = ()", e);
+            throw new CanNotConvertException(CAN_NOT_CONVERT);
+        }
+    }
+}
