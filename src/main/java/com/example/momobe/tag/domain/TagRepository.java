72b50a2a5a39782cdbef2c4c72e7b321a9dbae72
kkg5 <kkg05102@gmail.com>
2023-01-05 18:43:55 +0900

    feat : 태그 엔티티, findTagOrThrowException 작성

---
+++ src/main/java/com/example/momobe/tag/domain/TagRepository.java
@@ -0,0 +1,9 @@
+package com.example.momobe.tag.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByName(String name);
+}
