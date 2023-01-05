8eef53e32daf8007ca321c0dc6b894be557d711c
kkg5 <kkg05102@gmail.com>
2023-01-05 18:33:15 +0900

    feat : MeetingCommonService @Transactional 추가

--- src/main/java/com/example/momobe/meeting/application/MeetingCommonService.java
+++ src/main/java/com/example/momobe/meeting/application/MeetingCommonService.java
@@ -4,20 +4,22 @@ import com.example.momobe.common.exception.enums.ErrorCode;
 import com.example.momobe.meeting.domain.Meeting;
 import com.example.momobe.meeting.domain.MeetingNotFoundException;
 import com.example.momobe.meeting.domain.MeetingRepository;
-import com.example.momobe.reservation.domain.ReservationNotPossibleException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MeetingCommonService {
     private final MeetingRepository meetingRepository;
 
+    @Transactional(readOnly = true)
     public Meeting findMeetingOrThrowException(Long meetingId) {
         return meetingRepository.findById(meetingId)
                 .orElseThrow(() -> new MeetingNotFoundException(ErrorCode.DATA_NOT_FOUND));
     }
 
+    @Transactional
     public void closeMeeting(Long userId, Long meetingId) {
         findMeetingOrThrowException(meetingId).closeWithHostId(userId);
     }
