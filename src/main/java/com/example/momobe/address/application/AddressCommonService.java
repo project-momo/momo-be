3aa0a8fff51deb43e7dec6c3e78aa09796eea16f
kkg5 <kkg05102@gmail.com>
2023-01-06 15:28:00 +0900

    feat : verifyAddressIdsOrThrowException 메서드 작성

--- src/main/java/com/example/momobe/address/application/AddressCommonService.java
+++ src/main/java/com/example/momobe/address/application/AddressCommonService.java
@@ -1,12 +1,14 @@
 package com.example.momobe.address.application;
 
-import com.example.momobe.address.domain.Address;
 import com.example.momobe.address.domain.AddressNotFoundException;
 import com.example.momobe.address.domain.AddressRepository;
-import com.example.momobe.common.exception.enums.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.example.momobe.common.exception.enums.ErrorCode.DATA_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +16,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class AddressCommonService {
     private final AddressRepository addressRepository;
 
-    public Address findAddressOrThrowException(Long addressId) {
-        return addressRepository.findById(addressId)
-                .orElseThrow(() -> new AddressNotFoundException(ErrorCode.DATA_NOT_FOUND));
+    public void verifyAddressIdsOrThrowException(List<Long> addressIds) {
+        if (!addressRepository.existsAllByIdIn(addressIds)) {
+            throw new AddressNotFoundException(DATA_NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/example/momobe/address/domain/AddressRepository.java
+++ src/main/java/com/example/momobe/address/domain/AddressRepository.java
@@ -2,5 +2,8 @@ package com.example.momobe.address.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    Boolean existsAllByIdIn(List<Long> ids);
 }
