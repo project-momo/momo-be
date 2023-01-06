3c04421ad0ce5e33fab0a07b777801664eb97fc6
yangddoddi <pud1251@gmail.com>
2023-01-06 14:39:00 +0900

    update : Reservation에 예약 취소 메서드 추가

--- src/main/java/com/example/momobe/reservation/domain/Reservation.java
+++ src/main/java/com/example/momobe/reservation/domain/Reservation.java
@@ -1,11 +1,13 @@
 package com.example.momobe.reservation.domain;
 
 import com.example.momobe.common.domain.BaseTime;
+import com.example.momobe.meeting.domain.CanNotChangeMeetingStateException;
 import com.example.momobe.reservation.domain.enums.ReservationState;
 import lombok.*;
 
 import javax.persistence.*;
 
+import static com.example.momobe.common.exception.enums.ErrorCode.*;
 import static com.example.momobe.reservation.domain.enums.ReservationState.*;
 import static javax.persistence.EnumType.STRING;
 import static javax.persistence.GenerationType.*;
@@ -64,5 +66,23 @@ public class Reservation extends BaseTime {
 
     public ReservationState checkReservationState() {
         return this.reservationState;
+    }
+
+    /*
+     * 도메인 이벤트 발생 지점 (cancel, accept)
+     * 로직 완성 이후 주석 삭제 예정
+     * Author : yang_eun_chan
+     * Date : 2022/01/05
+     * */
+    public void cancel() {
+        if (this.reservationState == ACCEPT) throw new CanNotChangeReservationStateException(CONFIRMED_RESERVATION);
+
+        if (this.amount.match(0L)) this.reservationState = CANCEL;
+    }
+
+    public void accept() {
+        if (this.reservationState == CANCEL) throw new CanNotChangeMeetingStateException(CANCELED_RESERVATION);
+
+        this.reservationState = ACCEPT;
     }
 }
