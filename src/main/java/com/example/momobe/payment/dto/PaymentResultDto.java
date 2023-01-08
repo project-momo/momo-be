06eb82ffbc25560d449c21e3da4d6571c6527afc
yangddoddi <pud1251@gmail.com>
2023-01-08 18:06:05 +0900

    update : @EqualsAndHashcode 어노테이션 추가

--- src/main/java/com/example/momobe/payment/dto/PaymentResultDto.java
+++ src/main/java/com/example/momobe/payment/dto/PaymentResultDto.java
@@ -2,11 +2,13 @@ package com.example.momobe.payment.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@EqualsAndHashCode
 public class PaymentResultDto {
     private final String mId;
     private final String version;
