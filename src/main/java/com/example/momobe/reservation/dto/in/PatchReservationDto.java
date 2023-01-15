f38b89bc345d41318df6b7d78827effe314d2fe7
yangddoddi <pud1251@gmail.com>
2023-01-15 13:00:51 +0900

    update : 예약 승인/거절 시 반려 메세지 저장할 수 있도록 수정

--- src/main/java/com/example/momobe/reservation/dto/in/PatchReservationDto.java
+++ src/main/java/com/example/momobe/reservation/dto/in/PatchReservationDto.java
@@ -3,6 +3,7 @@ package com.example.momobe.reservation.dto.in;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -10,13 +11,21 @@ import javax.validation.constraints.Pattern;
 import static lombok.AccessLevel.*;
 
 @Getter
+@NoArgsConstructor(access = PRIVATE)
 public class PatchReservationDto {
     @NotBlank
     @Pattern(regexp = "^(?i)(true|false)$", message = "입력값은 대소문자 관계 없이 True 혹은 False여야 합니다.")
-    private final String isAccepted;
+    private String isAccepted;
 
-    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
-    public PatchReservationDto(@JsonProperty("isAccepted") String isAccepted) {
+    private String message;
+
+    public PatchReservationDto(String isAccepted,
+                               String message) {
+        this.isAccepted = isAccepted;
+        this.message = message;
+    }
+
+    public PatchReservationDto(String isAccepted) {
         this.isAccepted = isAccepted;
     }
 }
