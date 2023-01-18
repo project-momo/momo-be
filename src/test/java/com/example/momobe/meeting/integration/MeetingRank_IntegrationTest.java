d481ffe465b3cb65896b5828f8f34472593f6971
yangddoddi <pud1251@gmail.com>
2023-01-18 17:57:49 +0900

    test : 누락된 환경변수 설정 어노테이션 추가

--- src/test/java/com/example/momobe/meeting/integration/MeetingRank_IntegrationTest.java
+++ src/test/java/com/example/momobe/meeting/integration/MeetingRank_IntegrationTest.java
@@ -13,6 +13,7 @@ import com.example.momobe.user.domain.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,7 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
-//@EnabledIfEnvironmentVariable(named = "Local", matches = "local")
+@EnabledIfEnvironmentVariable(named = "Local", matches = "local")
 public class MeetingRank_IntegrationTest extends RedisTestConfig {
     @Autowired
     MockMvc mockMvc;
