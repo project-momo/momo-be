78d37711b8e598c85bd96f85521273e678c24d27
yangddoddi <pud1251@gmail.com>
2023-01-18 13:47:20 +0900

    refactor : 불필요한 주석 제거

--- src/test/java/com/example/momobe/common/util/ValidatorUtil.java
+++ src/test/java/com/example/momobe/common/util/ValidatorUtil.java
@@ -5,8 +5,7 @@ import javax.validation.Validator;
 
 public interface ValidatorUtil {
     Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-
-    // 같은 path의 값만 가져온다.
+    
     static <T> Object[] getArray(T dto, String path) {
         return validator.validate(dto).stream()
                 .filter(v -> v.getPropertyPath().toString().equals(path))
