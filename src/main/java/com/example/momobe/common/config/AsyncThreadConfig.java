114c58cdcc61542d202743a44354452615669f25
yangddoddi <pud1251@gmail.com>
2023-01-19 17:18:23 +0900

    feat : 비동기 쓰레드 풀 설정 추가

---
+++ src/main/java/com/example/momobe/common/config/AsyncThreadConfig.java
@@ -0,0 +1,25 @@
+package com.example.momobe.common.config;
+
+import lombok.Builder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@Configuration
+public class AsyncThreadConfig {
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("AsyncThread");
+        executor.initialize();
+        return executor;
+    }
+}
