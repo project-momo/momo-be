a5486b33c07cd1a47e770c1f1606e84c8dba7af7
yangddoddi <pud1251@gmail.com>
2023-01-24 15:32:31 +0900

    feat : 유저 메일 조회 dao 구현

---
+++ src/main/java/com/example/momobe/reservation/dao/UserMailQueryRepository.java
@@ -0,0 +1,32 @@
+package com.example.momobe.reservation.dao;
+
+import com.example.momobe.user.infrastructure.UnableToProcessException;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.Objects;
+
+import static com.example.momobe.common.exception.enums.ErrorCode.*;
+import static com.example.momobe.user.domain.QUser.*;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class UserMailQueryRepository {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public String findMailOf(Long userId) {
+        try {
+            return Objects.requireNonNull(jpaQueryFactory.select(user.email)
+                            .from(user)
+                            .where(user.id.eq(userId))
+                            .fetchOne())
+                    .getAddress();
+        } catch (Exception e) {
+            log.error("", e);
+            throw new UnableToProcessException(INVALID_EMAIL);
+        }
+    }
+}
