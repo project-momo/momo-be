1db2fdeb3b568eabd1c7a39d9256c1613a429739
yangddoddi <pud1251@gmail.com>
2023-01-24 15:25:53 +0900

    test : 이벤트 발행 테스트 추가

--- src/test/java/com/example/momobe/reservation/application/ReservationConfirmServiceTest.java
+++ src/test/java/com/example/momobe/reservation/application/ReservationConfirmServiceTest.java
@@ -3,11 +3,12 @@ package com.example.momobe.reservation.application;
 import com.example.momobe.common.resolver.UserInfo;
 import com.example.momobe.meeting.application.MeetingCommonService;
 import com.example.momobe.meeting.domain.Meeting;
-import com.example.momobe.reservation.domain.CanNotChangeReservationStateException;
-import com.example.momobe.reservation.domain.Money;
-import com.example.momobe.reservation.domain.Reservation;
-import com.example.momobe.reservation.domain.ReservationDate;
+import com.example.momobe.reservation.dao.UserMailQueryRepository;
+import com.example.momobe.reservation.domain.*;
 import com.example.momobe.reservation.dto.in.PatchReservationDto;
+import com.example.momobe.reservation.event.ReservationConfirmedEvent;
+import com.example.momobe.user.domain.Email;
+import com.example.momobe.user.domain.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDate;
@@ -38,6 +41,12 @@ class ReservationConfirmServiceTest {
     @Mock
     ReservationCommonService reservationCommonService;
 
+    @Mock
+    UserMailQueryRepository userMailQueryRepository;
+
+    @Mock
+    ApplicationEventPublisher applicationEventPublisher;
+
     private Meeting meeting;
 
     private UserInfo userInfo;
@@ -47,6 +56,8 @@ class ReservationConfirmServiceTest {
     private PatchReservationDto denyDto;
 
     private Reservation reservation;
+
+    private User reservedUser;
 
     @BeforeEach
     void init() {
@@ -73,6 +84,10 @@ class ReservationConfirmServiceTest {
                 .message("message")
                 .build();
 
+        reservedUser = User.builder()
+                .email(new Email(EMAIL2))
+                .build();
+
         reservation = Reservation.builder()
                 .reservationDate(ReservationDate.builder()
                         .date(LocalDate.now().plus(1, ChronoUnit.YEARS))
@@ -81,16 +96,21 @@ class ReservationConfirmServiceTest {
                         .build())
                 .amount(new Money(10000L))
                 .reservationState(PAYMENT_SUCCESS)
+                .reservedUser(new ReservedUser(reservedUser.getId()))
                 .build();
     }
 
+    @BeforeEach
+    void initApplicationEventListener() {
+        reservationConfirmService.setApplicationEventPublisher(applicationEventPublisher);
+    }
 
     @Test
     @DisplayName("불러온 meeting의 hostId와 인자의 hostId가 일치하지 않으면 예외 발생")
     void confirm_test1() {
         // given
         ReflectionTestUtils.setField(meeting, "hostId", userInfo.getId()+1);
-        given(meetingCommonService.getMeetingOrThrowException(any())).willReturn(meeting);
+        given(meetingCommonService.getMeeting(any())).willReturn(meeting);
 
         // when // then
         assertThatThrownBy(() -> reservationConfirmService.confirm(ID1, ID1, userInfo, denyDto))
@@ -101,8 +121,9 @@ class ReservationConfirmServiceTest {
     @DisplayName("dto의 isAccepted가 false일 경우 reservation의 state가 deny로 변경된다.")
     void confirm_test3() {
         //given
-        given(meetingCommonService.getMeetingOrThrowException(any())).willReturn(meeting);
-        given(reservationCommonService.getReservationOrThrowException(any())).willReturn(reservation);
+        given(meetingCommonService.getMeeting(any())).willReturn(meeting);
+        given(reservationCommonService.getReservation(any())).willReturn(reservation);
+        given(userMailQueryRepository.findMailOf(any())).willReturn(reservedUser.getEmail().getAddress());
 
         //when
         reservationConfirmService.confirm(meeting.getId(), reservation.getId(), userInfo, denyDto);
@@ -112,16 +133,47 @@ class ReservationConfirmServiceTest {
     }
 
     @Test
-    @DisplayName("dto의 isAccepted가 true일 경우 reservation의 state가 deny로 변경된다.")
+    @DisplayName("dto의 isAccepted가 true일 경우 reservation의 state가 accept로 변경된다.")
     void confirm_test4() {
         //given
-        given(meetingCommonService.getMeetingOrThrowException(any())).willReturn(meeting);
-        given(reservationCommonService.getReservationOrThrowException(any())).willReturn(reservation);
+        given(meetingCommonService.getMeeting(any())).willReturn(meeting);
+        given(reservationCommonService.getReservation(any())).willReturn(reservation);
+        given(userMailQueryRepository.findMailOf(any())).willReturn(reservedUser.getEmail().getAddress());
 
         //when
         reservationConfirmService.confirm(meeting.getId(), reservation.getId(), userInfo, acceptDto);
 
         //then
         assertThat(reservation.getReservationState()).isEqualTo(ACCEPT);
+    }
+
+    @Test
+    @DisplayName("로직이 성공적으로 실행될 경우 applicationEventPublisher가 1회 호출된다 (accept의 경우)")
+    void event_test1() {
+        //given
+        given(meetingCommonService.getMeeting(any())).willReturn(meeting);
+        given(reservationCommonService.getReservation(any())).willReturn(reservation);
+        given(userMailQueryRepository.findMailOf(any())).willReturn(reservedUser.getEmail().getAddress());
+
+        //when
+        reservationConfirmService.confirm(meeting.getId(), reservation.getId(), userInfo, acceptDto);
+
+        //then
+        verify(applicationEventPublisher, times(1)).publishEvent(any(ReservationConfirmedEvent.class));
+    }
+
+    @Test
+    @DisplayName("로직이 성공적으로 실행될 경우 applicationEventPublisher가 1회 호출된다 (deny의 경우)")
+    void event_test2() {
+        //given
+        given(meetingCommonService.getMeeting(any())).willReturn(meeting);
+        given(reservationCommonService.getReservation(any())).willReturn(reservation);
+        given(userMailQueryRepository.findMailOf(any())).willReturn(reservedUser.getEmail().getAddress());
+
+        //when
+        reservationConfirmService.confirm(meeting.getId(), reservation.getId(), userInfo, denyDto);
+
+        //then
+        verify(applicationEventPublisher, times(1)).publishEvent(any(ReservationConfirmedEvent.class));
     }
 }
