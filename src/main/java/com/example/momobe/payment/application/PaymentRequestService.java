b03e3103d5258de9844b8bd1ea54a80817647ac7
yangddoddi <pud1251@gmail.com>
2023-01-29 21:55:59 +0900

    update : 중괄호 누락된 것 추가

--- src/main/java/com/example/momobe/payment/application/PaymentRequestService.java
+++ src/main/java/com/example/momobe/payment/application/PaymentRequestService.java
@@ -21,7 +21,7 @@ import static org.springframework.http.MediaType.*;
 @Slf4j
 @Service
 public class PaymentRequestService extends PaymentApiService<PaymentResultDto> {
-    public PaymentRequestService(@Value("${payments.toss.secretKey") String secretKey, @Value("${payments.toss.url") String tossUrl, ApiService<PaymentResultDto> apiService) {
+    public PaymentRequestService(@Value("${payments.toss.secretKey") String secretKey, @Value("${payments.toss.url}") String tossUrl, ApiService<PaymentResultDto> apiService) {
         super(secretKey, tossUrl, apiService);
     }
 
