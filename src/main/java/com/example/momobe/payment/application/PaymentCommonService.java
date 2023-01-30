e69128b204b4607b3a63ef26e70106216a7c814c
yangddoddi <pud1251@gmail.com>
2023-01-30 21:44:33 +0900

    update : 불필요하게 긴 이름을 가진 예외명을 PaymentException으로 변경

--- src/main/java/com/example/momobe/payment/application/PaymentCommonService.java
+++ src/main/java/com/example/momobe/payment/application/PaymentCommonService.java
@@ -3,7 +3,7 @@ package com.example.momobe.payment.application;
 import com.example.momobe.common.exception.enums.ErrorCode;
 import com.example.momobe.payment.domain.Payment;
 import com.example.momobe.payment.domain.PaymentRepository;
-import com.example.momobe.payment.domain.UnableProceedPaymentException;
+import com.example.momobe.payment.domain.PaymentException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,11 +15,11 @@ public class PaymentCommonService {
 
     @Transactional(readOnly = true)
     public Payment getPayment(String paymentId) {
-        return paymentRepository.findPaymentByOrderId(paymentId).orElseThrow(() -> new UnableProceedPaymentException(ErrorCode.DATA_NOT_FOUND));
+        return paymentRepository.findPaymentByOrderId(paymentId).orElseThrow(() -> new PaymentException(ErrorCode.DATA_NOT_FOUND));
     }
 
     @Transactional(readOnly = true)
     public Payment getPayment(Long paymentId) {
-        return paymentRepository.findById(paymentId).orElseThrow(() -> new UnableProceedPaymentException(ErrorCode.DATA_NOT_FOUND));
+        return paymentRepository.findById(paymentId).orElseThrow(() -> new PaymentException(ErrorCode.DATA_NOT_FOUND));
     }
 }
--- src/main/java/com/example/momobe/payment/application/PaymentVerificationService.java
+++ src/main/java/com/example/momobe/payment/application/PaymentVerificationService.java
@@ -2,7 +2,7 @@ package com.example.momobe.payment.application;
 
 import com.example.momobe.common.exception.enums.ErrorCode;
 import com.example.momobe.payment.domain.Payment;
-import com.example.momobe.payment.domain.UnableProceedPaymentException;
+import com.example.momobe.payment.domain.PaymentException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,7 +13,7 @@ public class PaymentVerificationService {
     @Transactional(readOnly = true)
     public Boolean verify(Long amount, Payment payment) {
         if (!payment.matchAmount(amount)) {
-            throw new UnableProceedPaymentException(ErrorCode.AMOUNT_DOSE_NOT_MATCH);
+            throw new PaymentException(ErrorCode.AMOUNT_DOSE_NOT_MATCH);
         }
 
         return Boolean.TRUE;
