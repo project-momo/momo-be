3c078f3924ce7f3377ed4722c50b1f7bdbe1d01a
yangddoddi <pud1251@gmail.com>
2023-01-30 21:46:28 +0900

    update : 결제 성공 시 예약, 결제 정보 업데이트되도록 로직 추가

--- src/test/java/com/example/momobe/payment/application/PaymentProgressServiceTest.java
+++ src/test/java/com/example/momobe/payment/application/PaymentProgressServiceTest.java
@@ -2,6 +2,7 @@ package com.example.momobe.payment.application;
 
 
 import com.example.momobe.payment.domain.Payment;
+import com.example.momobe.payment.dto.PaymentResultDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,6 +25,9 @@ class PaymentProgressServiceTest {
 
     @Mock
     PaymentRequestService paymentRequestService;
+
+    @Mock
+    PaymentSuccessService paymentSuccessService;
 
     @InjectMocks
     PaymentProgressService paymentProgressService;
@@ -53,21 +57,6 @@ class PaymentProgressServiceTest {
         verify(paymentVerificationService, Mockito.times(1)).verify(1000L, payment);
     }
 
-//    @Test
-//    @DisplayName("requestPayment 1회 호출된다")
-//    void verifyTest3() {
-//        //given
-//        Payment payment = Payment.builder().build();
-//        given(paymentCommonService.getPaymentOrThrowException(ID)).willReturn(payment);
-//        given(paymentVerificationService.verify(1000L, payment)).willReturn(Boolean.TRUE);
-//
-//        //when
-//        paymentProgressService.progress(ID, ID, 1000L);
-//
-//        //then
-//        verify(paymentRequestService, Mockito.times(1)).requestPayment(ID, ID, 1000L);
-//    }
-
     @Test
     @DisplayName("함수가 전부 수행되고나면, payment에 인자로 받은 paymentKey 값이 저장된다.")
     void verifyTest4() {
@@ -82,21 +71,4 @@ class PaymentProgressServiceTest {
         //then
         assertThat(payment.getPaymentKey()).isEqualTo(ID);
     }
-
-//    @Test
-//    @DisplayName("함수가 전부 수행되고나면, paymentRequestService의 결과값을 리턴한다")
-//    void verifyTest5() {
-//        //given
-//        Payment payment = Payment.builder().build();
-//        PaymentResultDto dto = PaymentResultDto.builder().build();
-//        given(paymentCommonService.getPaymentOrThrowException(ID)).willReturn(payment);
-//        given(paymentVerificationService.verify(1000L, payment)).willReturn(Boolean.TRUE);
-//        given(paymentRequestService.requestPayment(ID, ID, 1000L)).willReturn(dto);
-//
-//        //when
-//        PaymentResultDto result = paymentProgressService.progress(ID, ID, 1000L);
-//
-//        //then
-//        assertThat(result).isEqualTo(dto);
-//    }
 }
