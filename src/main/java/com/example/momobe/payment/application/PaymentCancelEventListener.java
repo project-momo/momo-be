f1c4c6fc08937b02775cd8ab7479824b5a7ba16f
yangddoddi <pud1251@gmail.com>
2023-01-30 21:52:42 +0900

    update : 예외를 외부 예외에서 내부 예외(PaymentException)으로 변경

--- src/main/java/com/example/momobe/payment/application/PaymentCancelEventListener.java
+++ src/main/java/com/example/momobe/payment/application/PaymentCancelEventListener.java
@@ -1,8 +1,7 @@
 package com.example.momobe.payment.application;
 
-import com.example.momobe.common.exception.enums.ErrorCode;
 import com.example.momobe.payment.domain.Payment;
-import com.example.momobe.payment.domain.UnableProceedPaymentException;
+import com.example.momobe.payment.domain.PaymentException;
 import com.example.momobe.reservation.event.ReservationCanceledEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +15,8 @@ import org.springframework.transaction.event.TransactionalEventListener;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.example.momobe.common.exception.enums.ErrorCode.*;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -23,20 +24,21 @@ import java.util.Map;
 public class PaymentCancelEventListener {
     private final PaymentCancelService paymentCancelService;
     private final PaymentCommonService paymentCommonService;
+    private final Integer RETRY_COUNT = 3;
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT, classes = ReservationCanceledEvent.class)
     public void cancel(ReservationCanceledEvent event) {
         Map<String, Object> map = createMap(event);
 
-        for (int i=0; i<3; i++) {
+        for (int count=0; count < RETRY_COUNT; count++) {
             try {
                 paymentCancelService.process(event.getPaymentKey(), map);
                 Payment payment = paymentCommonService.getPayment(event.getPaymentId());
                 payment.cancel();
                 break;
             } catch (Exception e) {
-                log.error("결제 취소 요청 " + i + "번 실패", e);
+                log.error("결제 취소 요청 " + count + "번 실패", e);
                 sleep(100);
             }
         }
@@ -52,7 +54,7 @@ public class PaymentCancelEventListener {
         try {
             Thread.sleep(time);
         } catch (Exception e) {
-            throw new RuntimeException();
+            throw new PaymentException(UNABLE_TO_PROCESS);
         }
     }
 }
