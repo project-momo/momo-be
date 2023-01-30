93c75d2376ffe5481fb73fc7d6346b6e89920042
yangddoddi <pud1251@gmail.com>
2023-01-30 19:01:34 +0900

    update : 기존 DB와의 호환 문제로 제거되었던 상수 임시 복구

--- src/main/java/com/example/momobe/reservation/domain/enums/ReservationState.java
+++ src/main/java/com/example/momobe/reservation/domain/enums/ReservationState.java
@@ -6,5 +6,12 @@ public enum ReservationState {
     PAYMENT_SUCCESS,
     DENY,
     ACCEPT,
-    CANCEL
+    CANCEL,
+    /*
+    * 기존 DB에 저장된 Enum과의 호환 문제로 남겨놓았으나 추후 삭제 예정입니다.
+    * Author : Yang eun chan
+    * Date : 2023/01/30
+    * */
+    @Deprecated
+    SUCCESS,
 }
