8521f1cfaa72cd43f5d6d59bddd7d81869cafdd8
yangddoddi <pud1251@gmail.com>
2023-02-15 15:26:43 +0900

    update : paymentIdentification을 얻기 위한 메서드 추가

--- src/main/java/com/example/momobe/reservation/dao/PaymentDao.java
+++ src/main/java/com/example/momobe/reservation/dao/PaymentDao.java
@@ -1,7 +1,10 @@
 package com.example.momobe.reservation.dao;
 
+import com.example.momobe.reservation.dao.vo.PaymentIdentification;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface PaymentDao {
@@ -9,4 +12,14 @@ public interface PaymentDao {
             "from payment as p " +
             "where p.reservation_id = #{reservationId}")
     String findPaymentKeyByReservationId(Long reservationId);
+
+    @Select("select p.payment_key " +
+            "from payment as p " +
+            "where p.reservation_id in (#{reservationIds})")
+    List<String> findPaymentKeyByReservationIds(List<Long> reservationIds);
+
+    @Select("select p.payment_id, p.payment_key " +
+            "from payment as p " +
+            "where p.reservation_id in (#{reservationIds})")
+    List<PaymentIdentification> findPaymentIdentificationsByReservationIds(List<Long> reservationIds);
 }
