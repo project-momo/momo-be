62478787a431d7303f3fdfc3d260beecf3312a9f
yangddoddi <pud1251@gmail.com>
2023-02-17 22:45:38 +0900

    update : 기한 만료로 자동 거절될 경우 메일 발송되는 로직 추가

--- src/main/java/com/example/momobe/reservation/dao/UserDao.java
+++ src/main/java/com/example/momobe/reservation/dao/UserDao.java
@@ -5,6 +5,8 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
+import java.util.List;
+
 @Mapper
 public interface UserDao {
     @Select("SELECT user_point FROM user WHERE user_id = #{userId}")
@@ -13,4 +15,16 @@ public interface UserDao {
     @Update("UPDATE user SET user_point = user_point + #{pointValue} " +
             "WHERE user_id = #{userId}")
     void updateUserPoint(@Param("userId") Long userId, @Param("userPoint") Long userPoint);
+
+    @Select({
+            "<script>",
+            "SELECT u.address",
+            "FROM user as u",
+            "WHERE u.user_id IN",
+            "<foreach collection='userIds' item='userId' index='index' open='(' separator=',' close=')'>",
+            "#{userId}",
+            "</foreach>",
+            "</script>"
+    })
+    List<String> findUserMailByReservationUserId(@Param("userIds") List<Long> userIds);
 }
